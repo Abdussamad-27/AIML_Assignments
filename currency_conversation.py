37b3e8dc7840fe4706b05a7c1e8bef920889d27a
Abdussamad-27 <abdussamad27ab@gmail.com>
2026-07-04 14:11:19 +0530

    task 12 added

---
+++ currency_conversation.py
@@ -0,0 +1,15 @@
+#12 Currency Conversion for International Client Billing
+
+amount_in_inr=float(input("Enter amonut : "))
+conversion_rate=float(input("Enter Convertion rate : "))
+
+
+#calculating amount in usd 
+amount_in_usd =amount_in_inr/conversion_rate
+
+process_fee=0.02*amount_in_usd
+
+#calculating final amount  
+final_Amount=amount_in_usd+process_fee
+
+print(f"The client has to pay :{final_Amount:,.2f}$")
