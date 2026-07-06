b9a5adff726d8bdc389ab1b404cd7b47c1e90170
Abdussamad-27 <abdussamad27ab@gmail.com>
2026-07-06 15:36:13 +0530

    list task sheet task2 completed

---
+++ SalesRecord.py
@@ -0,0 +1,17 @@
+#Sales Record Cleanup
+
+Sales=[1200,1298,890,7600,678,77]
+print("Sales without Sorting :",Sales)
+
+
+#sorting in ascending order using sort()
+Sales.sort()
+print("Sales with Sorting :",Sales)
+
+
+#sorting in descending  order using reverse() to get highest sale
+
+highest_sale=Sales
+highest_sale.reverse()
+print("Sales in Descending order to show heighest sales first :",highest_sale)
+
