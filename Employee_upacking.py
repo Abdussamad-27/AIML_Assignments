2854ab27957d647a49598086d57ba448c5864a86
Abdussamad-27 <abdussamad27ab@gmail.com>
2026-07-07 08:22:11 +0530

    list task7 completed

---
+++ Employee_upacking.py
@@ -0,0 +1,20 @@
+#tuple packing and upacking
+
+employee=("emp_id","name","Department","Salary")
+
+#tuple upacking
+
+Emp_id,Name,Department,Salary=employee
+#initializing value
+Emp_id=23
+Name="Sam Wilson"
+Department="HR"
+Salary=38000
+
+
+#printing values
+
+print(f"Emp_id : {Emp_id}")
+print(f"Name : {Name}")
+print(f"Department : {Department}")
+print(f"Salary : {Salary}")
